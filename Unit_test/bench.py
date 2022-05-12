c5ab1a4ccdb94c5ef712b8c4990ff888cc26a25b
NEWUSER <l00169829@student.lyit.ie>
2022-05-12 19:49:22 +0100

    removed self

--- Unit_test/bench.py
+++ Unit_test/bench.py
@@ -6,8 +6,8 @@ from app import app
 import pytest
 import datetime
 
-def test_getaccomadation(benchmark, self):
-    tester = app.test_client(self)
+def test_getaccomadation(benchmark):
+    tester = app.test_client()
     tester.post(
     '/getaccomodations',
     data = dict(reservation_name="John Smith", reservation_date=datetime.date.today(), guests=4, nice_to_have="balcony"),
