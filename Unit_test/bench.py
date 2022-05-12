5ea3d78261653f3d50ccc914cb32124ba797da36
NEWUSER <l00169829@student.lyit.ie>
2022-05-12 19:34:02 +0100

    added parent to function call

--- Unit_test/bench.py
+++ Unit_test/bench.py
@@ -6,7 +6,7 @@ from app import app
 import pytest
 
 def test_getaccomadation(benchmark):
-    benchmark(getaccomodations({ "reservation_name":"John Smith", "reservation_date":"07/02/2023", "guests":"4", "nice_to_have":"sea view"}))
+    benchmark(app.getaccomodations({ "reservation_name":"John Smith", "reservation_date":"07/02/2023", "guests":"4", "nice_to_have":"sea view"}))
     print("Run")
 
 
