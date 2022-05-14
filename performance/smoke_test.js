a3ca872f4c9cadaa7d62a0a85ebb7a260a3e21c4
NEWUSER <l00169829@student.lyit.ie>
2022-05-14 12:50:16 +0100

    added smoke test.js

---
+++ performance/smoke_test.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export const options = {
+    vus: 1,
+    duration: '1m',
+
+    thresholds: {
+        http_req_duration: ['p(99)<1500'],
+    },
+};
+
+const BASE_URL = 'ec2-3-249-228-212.eu-west-1.compute.amazonaws.com:5001';
+const USERNAME = 'Gomez';
+const PASSWORD = 'Fester';
+
+export default () => {
+    const loginRes = http.post('${BASE_URL}/login', {
+        username: USERNAME,
+        password: PASSWORD,
+    });
+};
