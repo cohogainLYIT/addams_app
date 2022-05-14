4f5bb934bf33779d3e5b78f1717a472402e236ba
NEWUSER <l00169829@student.lyit.ie>
2022-05-14 13:52:43 +0100

    added form name

--- performance/smoke_test.js
+++ performance/smoke_test.js
@@ -3,7 +3,7 @@ import { check, group, sleep, fail } from 'k6';
 
 export const options = {
     vus: 1,
-    duration: '1m',
+    duration: '20s',
 
     thresholds: {
         http_req_duration: ['p(99)<1500'],
@@ -14,9 +14,15 @@ const BASE_URL = 'http://ec2-3-249-228-212.eu-west-1.compute.amazonaws.com:5001'
 const USERNAME = 'Gomez';
 const PASSWORD = 'Fester';
 
-export default () => {
-    const loginRes = http.post('http://ec2-3-249-228-212.eu-west-1.compute.amazonaws.com:5001/login/', {
-      username: USERNAME,
-      password: PASSWORD,
+
+export default function () {
+    // Request page containing a form
+    let res = http.get('http://ec2-3-249-228-212.eu-west-1.compute.amazonaws.com:5001/login/');
+  
+    // Now, submit form setting/overriding some fields of the form
+    res = res.submitForm({
+      formSelector: 'login',
+      fields: { username: USERNAME, password: PASSWORD },
     });
-};
+    sleep(3);
+  }
