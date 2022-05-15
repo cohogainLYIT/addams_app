48262041bda88690457f1dd87e27d6566500378b
NEWUSER <l00169829@student.lyit.ie>
2022-05-15 12:48:51 +0100

    added sleep and removed test duration

--- performance/smoke_test.js
+++ performance/smoke_test.js
@@ -1,16 +1,16 @@
 import http from 'k6/http';
 import { check, group, sleep, fail } from 'k6';
 
-export const options = {
-    vus: 1,
-    duration: '20s',
+//export const options = {
+//endregion    vus: 1,
+//    duration: '20s',
 
-    thresholds: {
-        http_req_duration: ['p(99)<1500'],
-    },
-};
+//    thresholds: {
+//        http_req_duration: ['p(99)<1500'],
+//    },
+//};
 
-const BASE_URL = 'http://ec2-3-249-228-212.eu-west-1.compute.amazonaws.com:5001';
+const BASE_URL = 'http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/';
 const USERNAME = 'Gomez';
 const PASSWORD = 'Fester';
 
@@ -18,7 +18,9 @@ const PASSWORD = 'Fester';
 export default function () {
     // Request page containing a form
     let res = http.get('http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/login/');
-  
+    
+    sleep(10);
+
     // Now, submit form setting/overriding some fields of the form
     res = res.submitForm({
       formSelector: 'form',
