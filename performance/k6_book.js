d0a2772a324d30cb09de799e1759cad34230d83b
NEWUSER <l00169829@student.lyit.ie>
2022-05-15 14:51:56 +0100

    added k6_book script and added to pipeline

---
+++ performance/k6_book.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export const options = {
+    vus: 5,
+    duration: '1m',
+
+    thresholds: {
+        http_req_duration: ['p(95)<500'],
+    },
+};
+
+const BASE_URL = 'http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/';
+  
+export function book() {
+    let response = http.get(`${BASE_URL}/:5001/`)
+    
+    sleep(5)
+  
+    response = http.post(`${BASE_URL}:5001/`,
+    {
+        reservation_name: 'Ciarán',
+        guests: '2',
+        reservation_date: '2022-05-31',
+        nice_to_have: 'wine and chocolate',
+    })
+
+    sleep(5)
+}
+  
