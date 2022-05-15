b44cbdab2f99e6bc09b1c097fdf27d4dc10ec297
NEWUSER <l00169829@student.lyit.ie>
2022-05-15 13:55:29 +0100

    added recorded script from K6 record

--- performance/smoke_test.js
+++ performance/smoke_test.js
@@ -15,10 +15,33 @@ const USERNAME = 'Gomez';
 const PASSWORD = 'Fester';
 
 
-export default function () {
-    // Request page containing a form
-    let loginPageResponse = http.post('http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/login/', {
-        username: USERNAME,
-        password: PASSWORD,
-    });
-};
+export function login() {
+    let response
+  
+    group(
+      'page_1 - http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/login/',
+      function () {
+        response = http.get('http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/login/', {
+          headers: {
+            'upgrade-insecure-requests': '1',
+          },
+        })
+        sleep(6.2)
+  
+        response = http.post(
+          'http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/login/',
+          {
+            username: 'Gomez',
+            password: 'Fester',
+          },
+          {
+            headers: {
+              'content-type': 'application/x-www-form-urlencoded',
+              origin: 'http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001',
+              'upgrade-insecure-requests': '1',
+            },
+          }
+        )
+      }
+    )
+  }
