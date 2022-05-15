7bf53c2d69e32e199f81cb2bcc706a7cfd36b88f
NEWUSER <l00169829@student.lyit.ie>
2022-05-15 14:30:30 +0100

    added book function to k6 smoke_test

--- performance/smoke_test.js
+++ performance/smoke_test.js
@@ -15,15 +15,33 @@ const USERNAME = 'Gomez';
 const PASSWORD = 'Fester';
 
 
-export default function login() {
+export function login() {
     let response = http.get('http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/login/')
     
-    sleep(6.2)
+    sleep(5)
   
     response = http.post('http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/login/',
     {
         username: 'Gomez',
         password: 'Fester',
     })
+
+    sleep(5)
+}
+  
+export function book() {
+    let response = http.get('http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/')
+    
+    sleep(5)
+  
+    response = http.post('http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/',
+    {
+        reservation_name: 'Ciarán',
+        guests: '2',
+        reservation_date: '2022-05-31',
+        nice_to_have: 'wine and chocolate',
+    })
+
+    sleep(5)
 }
   
