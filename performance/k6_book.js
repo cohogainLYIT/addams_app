1e98bb73f4c9fc2405183db4ad13d00a63e39853
NEWUSER <l00169829@student.lyit.ie>
2022-05-15 16:21:45 +0100

    changed base url

--- performance/k6_book.js
+++ performance/k6_book.js
@@ -13,7 +13,7 @@ export const options = {
 const BASE_URL = 'http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/';
   
 export default function book() {
-    let response = http.get(`${BASE_URL}/:5001/`)
+    let response = http.get(`${BASE_URL}:5001/`)
     
     sleep(5)
   
