9da961009cfb5ae5d13ca0f70557d45a55f172e3
NEWUSER <l00169829@student.lyit.ie>
2022-05-15 13:01:18 +0100

    added const test duration

--- performance/smoke_test.js
+++ performance/smoke_test.js
@@ -1,14 +1,14 @@
 import http from 'k6/http';
 import { check, group, sleep, fail } from 'k6';
 
-//export const options = {
-//endregion    vus: 1,
-//    duration: '20s',
+export const options = {
+    vus: 1,
+    duration: '10s',
 
-//    thresholds: {
-//        http_req_duration: ['p(99)<1500'],
-//    },
-//};
+    thresholds: {
+        http_req_duration: ['p(99)<3000'],
+    },
+};
 
 const BASE_URL = 'http://ec2-52-213-198-226.eu-west-1.compute.amazonaws.com:5001/';
 const USERNAME = 'Gomez';
