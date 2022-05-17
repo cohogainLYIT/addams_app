6f5fd52c37a451e763b4bec4bb9b4692ed968101
NEWUSER <l00169829@student.lyit.ie>
2022-05-17 22:26:35 +0100

    .

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -91,6 +91,35 @@ window.BENCHMARK_DATA = {
           "range": "0",
           "extra": "0"
         }]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "committer": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "distinct": true,
+          "id": "5bbea4ba9cb0da00393b61e3fb969f0cff9b5487",
+          "message": "removed comments to trigger workflow again",
+          "timestamp": "2022-05-17T22:16:27+01:00",
+          "tree_id": "13d293c46c186d97a3be00086d0718e52966df91",
+          "url": "https://github.com/cohogainLYIT/addams_app/commit/5bbea4ba9cb0da00393b61e3fb969f0cff9b5487"
+        },
+        "date": 1652822395855,
+        "tool": "k6_login",
+        "benches":[{
+          "name": "Login",
+          "value": 130.154449,
+          "unit": "ms",
+          "range": "0",
+          "extra": "0"
+        }]
       }
     ]
   }
