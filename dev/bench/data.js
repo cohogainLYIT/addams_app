09226db341ae717d53d1e3dacde7a64517bcf105
github-action-benchmark <github@users.noreply.github.com>
2022-05-17 20:10:49 +0000

    add K6 benchmark (k6) benchmark result for 4fb29916473bb081bb040e706a4d5a69200cae72

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1652818249147,
+  "repoUrl": "https://github.com/cohogainLYIT/addams_app",
+  "entries": {
+    "K6 benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "committer": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "distinct": true,
+          "id": "4fb29916473bb081bb040e706a4d5a69200cae72",
+          "message": "remove output.json",
+          "timestamp": "2022-05-17T21:08:24+01:00",
+          "tree_id": "cb6d4aa312d567876743f70db2cb8ef0d4b729c1",
+          "url": "https://github.com/cohogainLYIT/addams_app/commit/4fb29916473bb081bb040e706a4d5a69200cae72"
+        },
+        "date": 1652818247867,
+        "tool": "k6",
+        "benches": {
+          "name": "k6 test",
+          "unit": "ms",
+          "range": "0",
+          "extra": "0"
+        }
+      }
+    ]
+  }
+}
