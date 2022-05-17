e55cd49e878fcf7cedab58c584cec5cf4137c992
github-action-benchmark <github@users.noreply.github.com>
2022-05-17 21:00:58 +0000

    add K6 Login benchmark (k6_login) benchmark result for 1283e59189170c8cd043b875ae32d9ca40e6ace2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1652821257067,
+  "lastUpdate": 1652821258781,
   "repoUrl": "https://github.com/cohogainLYIT/addams_app",
   "entries": {
     "K6 Book benchmark": [
@@ -26,6 +26,36 @@ window.BENCHMARK_DATA = {
         "tool": "k6_book",
         "benches": {
           "name": "Book",
+          "unit": "ms",
+          "range": "0",
+          "extra": "0"
+        }
+      }
+    ],
+    "K6 Login benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "committer": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "distinct": true,
+          "id": "1283e59189170c8cd043b875ae32d9ca40e6ace2",
+          "message": "added login k6 benchmark to workflow file",
+          "timestamp": "2022-05-17T21:46:14+01:00",
+          "tree_id": "5b0b15b203addcee82c82c7640a66c4ce321adcc",
+          "url": "https://github.com/cohogainLYIT/addams_app/commit/1283e59189170c8cd043b875ae32d9ca40e6ace2"
+        },
+        "date": 1652821258070,
+        "tool": "k6_login",
+        "benches": {
+          "name": "Login",
           "unit": "ms",
           "range": "0",
           "extra": "0"
