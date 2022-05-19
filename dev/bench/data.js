1c2a636044c2d16d3a080880d2aff9f7495eb38d
github-action-benchmark <github@users.noreply.github.com>
2022-05-19 14:48:03 +0000

    add K6 Book benchmark (k6_book) benchmark result for 976e89c6c4708badb17a313f50035224394e1c9a

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1652971683365,
+  "repoUrl": "https://github.com/cohogainLYIT/addams_app",
+  "entries": {
+    "K6 Book benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "committer": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "distinct": true,
+          "id": "976e89c6c4708badb17a313f50035224394e1c9a",
+          "message": "removed test suite",
+          "timestamp": "2022-05-19T15:44:16+01:00",
+          "tree_id": "9a0c9a087b10b4cf2fff6dc9698710be9569bfa9",
+          "url": "https://github.com/cohogainLYIT/addams_app/commit/976e89c6c4708badb17a313f50035224394e1c9a"
+        },
+        "date": 1652971682205,
+        "tool": "k6_book",
+        "benches": {
+          "name": "Book",
+          "value": 427.076172,
+          "unit": "ms",
+          "range": "0",
+          "extra": "0"
+        }
+      }
+    ]
+  }
+}
