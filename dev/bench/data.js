f9aae63f07e26d43fbd153bae47e01cb31563411
github-action-benchmark <github@users.noreply.github.com>
2022-05-19 16:52:22 +0000

    add K6 Login benchmark (k6_login) benchmark result for 976e89c6c4708badb17a313f50035224394e1c9a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1652979141049,
+  "lastUpdate": 1652979142922,
   "repoUrl": "https://github.com/cohogainLYIT/addams_app",
   "entries": {
     "K6 Book benchmark": [
@@ -161,6 +161,39 @@ window.BENCHMARK_DATA = {
             "extra": "0"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "committer": {
+            "email": "l00169829@student.lyit.ie",
+            "name": "NEWUSER",
+            "username": "cohogainLYIT"
+          },
+          "distinct": true,
+          "id": "976e89c6c4708badb17a313f50035224394e1c9a",
+          "message": "removed test suite",
+          "timestamp": "2022-05-19T15:44:16+01:00",
+          "tree_id": "9a0c9a087b10b4cf2fff6dc9698710be9569bfa9",
+          "url": "https://github.com/cohogainLYIT/addams_app/commit/976e89c6c4708badb17a313f50035224394e1c9a"
+        },
+        "date": 1652979141965,
+        "tool": "k6_login",
+        "benches": {
+          "benches": [
+            {
+              "name": "Login",
+              "value": 127.394407,
+              "unit": "ms",
+              "range": "0",
+              "extra": "0"
+            }
+          ]
+        }
       }
     ]
   }
